c586297a73f573aff2bc86ed4be73daca7c9a6f8
Lea Brunori <lbrunori.utbm@gmail.com>
2020-03-07 19:20:17 +0100

    display "--" when nutriment unknown

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -2,7 +2,11 @@ class Burger < ApplicationRecord
   def nutriments
     @product = Openfoodfacts::Product.get(code, locale: 'fr')
     @nutr =  @product.nutriments
-    @res = ["fat" => @nutr.fat, "sugars" => @nutr.sugars, "salt" => @nutr.salt, "proteins" => @nutr.proteins]
+    if @nutr
+      @res = ["fat" => @nutr.fat, "sugars" => @nutr.sugars, "salt" => @nutr.salt, "proteins" => @nutr.proteins]
+    else
+      @res = ["fat" => "--", "sugars" => "--", "salt" => "--", "proteins" => "--"]
+    end
   end
 
 end
