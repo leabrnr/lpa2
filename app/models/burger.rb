2023738b9b8264056bb4083937335228906f7505
Lea Brunori <lbrunori.utbm@gmail.com>
2020-03-07 18:58:57 +0100

    added nutriments dans fiche burger et JSONs

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,2 +1,8 @@
 class Burger < ApplicationRecord
+  def nutriments
+    @product = Openfoodfacts::Product.get(code, locale: 'fr')
+    @nutr =  @product.nutriments
+    @res = ["fat" => @nutr.fat, "sugars" => @nutr.sugars, "salt" => @nutr.salt, "proteins" => @nutr.proteins]
+  end
+
 end
